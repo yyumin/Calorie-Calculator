cf3e9180944b30cc8b86ff2dff26d1cd2173c8b0
chenyumin <ychen317@students.cs.ubc.ca>
2023-02-26 19:26:17 -0800

    Phrase 1 version 4

--- src/test/model/FoodIntakeTest.java
+++ src/test/model/FoodIntakeTest.java
@@ -1,5 +1,25 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FoodIntakeTest {
-    // delete or rename this class!
+    private FoodIntake testFoodIntake;
+
+    @BeforeEach
+    void setup() {
+        testFoodIntake = new FoodIntake("eggs", 100, 10, 10, 10);
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("eggs", testFoodIntake.getName());
+        assertEquals(100, testFoodIntake.getCal());
+        assertEquals(10, testFoodIntake.getCarbs());
+        assertEquals(10, testFoodIntake.getFat());
+        assertEquals(10, testFoodIntake.getProtein());
+    }
+
 }
